fc2cdbfb41255916adc1757057b235a835fdc8b8
nrkey <nrkey@iastate.edu>
2020-01-31 13:46:59 -0600

    got switch working still figuring out changing background

--- Experiments/Nathan Key/darkmodeTest/app/src/main/java/com/example/darkmode/MainActivity.java
+++ Experiments/Nathan Key/darkmodeTest/app/src/main/java/com/example/darkmode/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.darkmode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Switch sw = (Switch) findViewById(R.id.darkSwitch);
+        //RelativeLayout lo = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        setOnCLick(sw);//, lo);
+
+    }
+    private void setOnCLick(final Switch sw){//}, final RelativeLayout lo){
+        sw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sw.isChecked()){
+                    sw.setText("ON");
+                   // lo.setBackgroundColor(Color.parseColor("@color/darkMode"));
+                }
+                else{
+                    sw.setText("Off"); //  lo.setBackgroundColor(Color.parseColor("@color/lightMode"));
+                }
+            }
+        });
     }
 }
