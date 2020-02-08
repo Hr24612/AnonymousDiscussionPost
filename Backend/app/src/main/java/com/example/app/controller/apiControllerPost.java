f7ff2e82b7f8cec0fe14c76794d197c0ba71f9b3
Nick'sMacBook <battaninic@gmail.com>
2020-02-07 18:41:33 -0600

    commented out the things that broke it, working with this revision

--- Backend/app/src/main/java/com/example/app/controller/apiControllerPost.java
+++ Backend/app/src/main/java/com/example/app/controller/apiControllerPost.java
@@ -8,60 +8,60 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class apiControllerPost {
 
-    @Autowired
-    postRepo repository;
-
-    //Need to figure out a way to not request literally everything to add a post
-    @PostMapping
-    public void addPost(@RequestParam String title, String body, String creator, String hashtag, int likes, int dislikes){
-        Post newPost = new Post(title, body, creator, hashtag,likes,dislikes);
-        //figure out how to save to repo
-        //repository.save(newPost);
-    }
-
-    //Need to add functionality to receive posts
-    @GetMapping
-    public void getPost(){
-
-    }
-
-    @GetMapping
-    public void getAllPost(){
-
-    }
-
-    //I assume I have to create a get request for sorting by hashtags, new, and score
-
-    @GetMapping
-    public void getPostsByHashtag(@RequestParam("hashtag") String hashtag){
-        Post allPosts = new Post("","","",hashtag,0,0);
-
-        //help
-//        for(Post cust: repository.findByHashtag(hashtag)){
-//            allPosts += cust.toPost();
-//        }
-    }
-
-    @GetMapping
-    public void getPostByNew(){
-
-    }
-
-    @GetMapping
-    public void getPostsByHighestScore(){
-
-    }
-
-    //Delete a post
-    @DeleteMapping
-    public void deletePost(){
-
-    }
-
-    //Update a post (probably only going to let them update the body of the post?
-    @PutMapping
-    public void updatePost(){
-
-    }
+//    @Autowired
+//    postRepo repository;
+//
+//    //Need to figure out a way to not request literally everything to add a post
+//    @PostMapping
+//    public void addPost(@RequestParam String title, String body, String creator, String hashtag, int likes, int dislikes){
+//        Post newPost = new Post(title, body, creator, hashtag,likes,dislikes);
+//        //figure out how to save to repo
+//        //repository.save(newPost);
+//    }
+//
+//    //Need to add functionality to receive posts
+//    @GetMapping
+//    public void getPost(){
+//
+//    }
+//
+//    @GetMapping
+//    public void getAllPost(){
+//
+//    }
+//
+//    //I assume I have to create a get request for sorting by hashtags, new, and score
+//
+//    @GetMapping
+//    public void getPostsByHashtag(@RequestParam("hashtag") String hashtag){
+//        Post allPosts = new Post("","","",hashtag,0,0);
+//
+//        //help
+////        for(Post cust: repository.findByHashtag(hashtag)){
+////            allPosts += cust.toPost();
+////        }
+//    }
+//
+//    @GetMapping
+//    public void getPostByNew(){
+//
+//    }
+//
+//    @GetMapping
+//    public void getPostsByHighestScore(){
+//
+//    }
+//
+//    //Delete a post
+//    @DeleteMapping
+//    public void deletePost(){
+//
+//    }
+//
+//    //Update a post (probably only going to let them update the body of the post?
+//    @PutMapping
+//    public void updatePost(){
+//
+//    }
 
 }
--- Backend/app/src/main/java/com/example/app/repo/postRepo.java
+++ Backend/app/src/main/java/com/example/app/repo/postRepo.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 
 public interface postRepo extends CrudRepository<Post,Long> {
-    //I have no idea how to actually declare what these do//
-
-    //Find posts by a hashtag
-    List<Post> findByHashtag(String hashtag);
-
-    //Sort posts by newest
-    List<Post> sortByNew();
-
-    //Sort posts by highest score
-    List<Post> sortByHighestScore();
+//    //I have no idea how to actually declare what these do//
+//
+//    //Find posts by a hashtag
+//    List<Post> findByHashtag(String hashtag);
+//
+//    //Sort posts by newest
+//    List<Post> sortByNew();
+//
+//    //Sort posts by highest score
+//    List<Post> sortByHighestScore();
 }
