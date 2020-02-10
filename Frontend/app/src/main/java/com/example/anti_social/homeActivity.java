5093c58975b8fb7ee14585863737967d3e61e45c
mphipps <mphipps@iastate.edu>
2020-02-10 10:40:38 -0600

    Implemented GET request on dummy data

--- Frontend/app/src/main/java/com/example/anti_social/homeActivity.java
+++ Frontend/app/src/main/java/com/example/anti_social/homeActivity.java
@@ -1,19 +1,40 @@
 package com.example.anti_social;
+// Helpful link for testing with json http://myjson.com/
 
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class homeActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+public class homeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         if(getIntent().hasExtra("Mainactivity.name")){
-            TextView testTV = (TextView) findViewById(R.id.testTV);
-            testTV.setText(getIntent().getExtras().getString("Mainactivity.name"));
+            final TextView testTV = (TextView) findViewById(R.id.testTV);
+            RequestQueue queue = Volley.newRequestQueue(homeActivity.this);
+            String url ="https://api.myjson.com/bins/11ujto";
+            //This Method will get the full json response for the get request with the url given above. e.g. may need to be parsed
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    testTV.setText("Reponse : " + response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    testTV.setText("Reponse : 404");
+                }
+            });
+            queue.add(stringRequest);
         }
     }
 }
