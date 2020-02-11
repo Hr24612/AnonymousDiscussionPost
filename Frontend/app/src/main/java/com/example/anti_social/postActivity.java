e0bacbb9982a1622b9b88ae20fc805b100ef9bbd
nrkey <nrkey@iastate.edu>
2020-02-11 15:41:25 -0600

    added String get requests in post

--- Frontend/app/src/main/java/com/example/anti_social/postActivity.java
+++ Frontend/app/src/main/java/com/example/anti_social/postActivity.java
@@ -1,7 +1,18 @@
 package com.example.anti_social;
+
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.example.anti_social.app.AppController;
+import com.example.anti_social.net_utils.Const;
+
 
 public class postActivity extends AppCompatActivity {
 
@@ -9,8 +20,24 @@ public class postActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        
 
-       // StringRequest strReq = new StringRequest(Method.GET, Const.POSTMAN_URL)
+        StringRequest strReq = new StringRequest(Request.Method.GET, Const.POSTMAN_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(AppController.TAG, response.toString());
+                //pDialog.hide();
 
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(AppController.TAG, "Error: " + error.getMessage());
+                // pDialog.hide();
+            }
+
+        });
+        AppController.getInstance().addToRequestQueue(strReq, "testTag");
     }
 }
