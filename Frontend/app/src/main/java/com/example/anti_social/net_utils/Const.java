ffba15f4ece1e10fab0189a8f5b23b118b732126
nrkey <nrkey@iastate.edu>
2020-02-11 12:46:34 -0600

    added files from volley tutorial

---
+++ Frontend/app/src/main/java/com/example/anti_social/net_utils/Const.java
@@ -0,0 +1,6 @@
+package com.example.anti_social.net_utils;
+
+public class Const {
+    public static final String POSTMAN_URL = "https://docs.postman-echo.com/?version=latest";
+    public static final String LOCAL_HOST = "http://10.0.2.2";
+}
