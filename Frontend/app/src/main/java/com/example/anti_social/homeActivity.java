75ea3f5c54dfe99da56a32adc631ba51693ba4ac
Matthew P <mphipps@iastate.edu>
2020-02-12 23:34:44 -0600

    Created RecyclerViewAdapter class to implement infinite scrolling for #3

--- Frontend/app/src/main/java/com/example/anti_social/homeActivity.java
+++ Frontend/app/src/main/java/com/example/anti_social/homeActivity.java
@@ -2,20 +2,16 @@ package com.example.anti_social;
 // Helpful link for testing with json http://myjson.com/
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import java.util.ArrayList;
 
 public class homeActivity extends AppCompatActivity {
+    private ArrayList<String> postTitles = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,22 +20,17 @@ public class homeActivity extends AppCompatActivity {
             RecyclerView postRecyler = (RecyclerView) findViewById(R.id.homePostViewRV);
             RecyclerView.LayoutManager postLayoutManager = new LinearLayoutManager(this);
             postRecyler.setLayoutManager(postLayoutManager);
-            final TextView testTV = (TextView) findViewById(R.id.testTV);
-            RequestQueue queue = Volley.newRequestQueue(homeActivity.this);
-            String url ="https://api.myjson.com/bins/11ujto";
-            //This Method will get the full json response for the get request with the url given above. e.g. may need to be parsed
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    testTV.setText("Reponse : " + response);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    testTV.setText("Reponse : 404");
-                }
-            });
-            queue.add(stringRequest);
+            for(int i = 0; i < 50;i++){
+                postTitles.add("Item " + i);
+            }
+            initRecyclerView();
         }
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.homePostViewRV);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(postTitles, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
