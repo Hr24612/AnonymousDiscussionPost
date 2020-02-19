283bbb2887e3d30ccf422f70089787c903454067
nrkey <nrkey@iastate.edu>
2020-02-19 16:28:30 -0600

    created demo3 branch

--- Frontend/app/src/main/java/com/example/anti_social/CreatePostActivity.java
+++ Frontend/app/src/main/java/com/example/anti_social/CreatePostActivity.java
@@ -27,6 +27,7 @@ public class CreatePostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
         Log.d("CPActivityStart","Creating CreatePostActivity");
