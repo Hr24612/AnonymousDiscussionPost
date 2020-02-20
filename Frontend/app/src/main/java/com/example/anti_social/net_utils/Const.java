f06a089210d2d0387e1fcc4c7e8fcdb9d02473d3
nrkey <nrkey@iastate.edu>
2020-02-20 15:41:41 -0600

    test again

--- Frontend/app/src/main/java/com/example/anti_social/net_utils/Const.java
+++ Frontend/app/src/main/java/com/example/anti_social/net_utils/Const.java
@@ -7,4 +7,5 @@ public class Const {
     public static final String POSTMAN_URL = "https://postman-echo.com/get?foo1=bar1&foo2=bar2";
     public static final String LOCAL_HOST = "http://10.0.2.2";
     public static final String CREATEPOST = "http://coms-309-sk-4.cs.iastate.edu:8080/postApi/createPost";
+    //add more urls
 }
