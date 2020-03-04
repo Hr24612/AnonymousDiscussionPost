83511463e9e5a38093b11ded3cc58a40a37949e3
Nick'sMacBook <battaninic@gmail.com>
2020-03-04 11:41:20 -0600

    test

--- Backend/app/src/main/java/com/example/app/controller/apiControllerComment.java
+++ Backend/app/src/main/java/com/example/app/controller/apiControllerComment.java
@@ -1,145 +1,145 @@
-//package com.example.app.controller;
-//import com.example.app.exception.ResourceNotFoundException;
-//import com.example.app.exception.UserNotFoundException;
-//import com.example.app.model.comment;
-//import com.example.app.repo.commentRepo;
-//import com.example.app.repo.postRepo;
-//import com.example.app.repo.userRepo;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import javax.validation.Valid;
-//import java.util.List;
-//
-///**
-// * API class for managing comments on a post
-// */
-//
-//@RestController
-//@RequestMapping("/api/post")
-//public class apiControllerComment {
-//
-//    //Reference to commentRepo interface
-//    @Autowired
-//    commentRepo commentRepo;
-//
-//    //Reference to userRepo interface
-//    @Autowired
-//    postRepo postRepo;
-//
-//    //Reference to userRepo interface
-//    @Autowired
-//    userRepo userRepo;
-//
-//
-//    //*****************//
-//
-//    /***********************/
-//    /**** GET Comment ****/
-//    /**********************/
-//
-//    //Get comments of a post
-//    @GetMapping("/{postId}/commentsByPostId")
-//    public List<comment> getAllCommentsByPostId(@PathVariable (value = "postId") Long postId) {
-//        return commentRepo.findByPostId(postId);
-//    }
-//
-//    //Get all comments by a user
-//    @GetMapping("/{userId}/commentsByUserId")
-//    public List<comment> getAllCommentsByUserId(@PathVariable (value = "userId") Long userId) {
-//        return commentRepo.findByUserID(userId);
-//    }
-//
-//    /*********************/
-//    /** END GET Comment **/
-//    /*********************/
-//
-//    //*****************//
-//
-//    /***********************/
-//    /**** POST Comments ***/
-//    /**********************/
-//
-//    //Post a comment with userId and postId
-//    @PostMapping("{userId}/{postId}/comment")
-//    public comment createComment(@PathVariable (value = "userId") Long userId,
-//                                 @PathVariable (value = "postId") Long postId,
-//                                 @Valid @RequestBody comment comment) throws UserNotFoundException {
-//        if(!userRepo.existsById(userId)){
-//            throw new UserNotFoundException(userId);
-//        }
-//        else{
-//            return postRepo.findById(postId).map(post -> {
-//                comment.setUser(userRepo.findByID(userId));
-//                comment.setPost(post);
-//                return commentRepo.save(comment);
-//            }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
-//        }
-//    }
-//
-//    /**********************/
-//    /** END POST Comment **/
-//    /**********************/
-//
-//    //*****************//
-//
-//    /*********************/
-//    /**** PUT Comments ***/
-//    /*********************/
-//
-//    //Update comment with userId and postId
-//    @PutMapping("{userId}/{postId}/updateComment/{commentId}")
-//    public comment updateComment(@PathVariable (value = "userId") Long userId,
-//                                 @PathVariable (value = "postId") Long postId,
-//                                 @PathVariable (value = "commentId") Long commentId,
-//                                 @Valid @RequestBody comment commentRequest) throws UserNotFoundException {
-//        System.out.println(userRepo.findByUserName("hr24612"));
-//        if(!userRepo.existsById(userId)) {
-//            throw new UserNotFoundException(userId);
-//        }
-//        else if(!postRepo.existsById(postId)) {
-//            throw new ResourceNotFoundException("PostId " + postId + " not found");
-//        }
-//        else{
-//            return commentRepo.findById(commentId).map(comment -> {
-//                comment.setText(commentRequest.getText());
-//                return commentRepo.save(comment);
-//            }).orElseThrow(() -> new ResourceNotFoundException("CommentId " + commentId + "not found"));
-//        }
-//    }
-//
-//    /*********************/
-//    /** END PUT Comment **/
-//    /*********************/
-//
-//    //*****************//
-//
-//    /*********************/
-//    /*** DELETE Comment **/
-//    /*********************/
-//
-//    //Delete a comment with userId and postId
-//    @DeleteMapping("{userId}/{postId}/comment/{commentId}")
-//    public ResponseEntity<?> deleteComment(@PathVariable (value = "userId") Long userId,
-//                                           @PathVariable (value = "postId") Long postId,
-//                                           @PathVariable (value = "commentId") Long commentId) throws UserNotFoundException {
-//
-//            return commentRepo.findById(commentId).map(comment -> {
-//                if(comment.getUser().getId().equals(userId) && comment.getPost().getId().equals(postId)){
-//                    commentRepo.delete(comment);
-//                   return new ResponseEntity<>("Deleted!", HttpStatus.OK);
-//                }
-//                else {
-//                    throw new ResourceNotFoundException("userId: " + userId + " or " + " postId: " + postId + " were incorrect");
-//                }
-//            }).orElseThrow(() -> new ResourceNotFoundException("Comment not found with id " + commentId));
-//    }
-//
-//    /************************/
-//    /** END DELETE Comment **/
-//    /************************/
-//
-//    //*****************//
-//
-//}
+package com.example.app.controller;
+import com.example.app.exception.ResourceNotFoundException;
+import com.example.app.exception.UserNotFoundException;
+import com.example.app.model.comment;
+import com.example.app.repo.commentRepo;
+import com.example.app.repo.postRepo;
+import com.example.app.repo.userRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * API class for managing comments on a post
+ */
+
+@RestController
+@RequestMapping("/api/post")
+public class apiControllerComment {
+
+    //Reference to commentRepo interface
+    @Autowired
+    commentRepo commentRepo;
+
+    //Reference to userRepo interface
+    @Autowired
+    postRepo postRepo;
+
+    //Reference to userRepo interface
+    @Autowired
+    userRepo userRepo;
+
+
+    //*****************//
+
+    /***********************/
+    /**** GET Comment ****/
+    /**********************/
+
+    //Get comments of a post
+    @GetMapping("/{postId}/commentsByPostId")
+    public List<comment> getAllCommentsByPostId(@PathVariable (value = "postId") Long postId) {
+        return commentRepo.findByPostId(postId);
+    }
+
+    //Get all comments by a user
+    @GetMapping("/{userId}/commentsByUserId")
+    public List<comment> getAllCommentsByUserId(@PathVariable (value = "userId") Long userId) {
+        return commentRepo.findByUserID(userId);
+    }
+
+    /*********************/
+    /** END GET Comment **/
+    /*********************/
+
+    //*****************//
+
+    /***********************/
+    /**** POST Comments ***/
+    /**********************/
+
+    //Post a comment with userId and postId
+    @PostMapping("{userId}/{postId}/comment")
+    public comment createComment(@PathVariable (value = "userId") Long userId,
+                                 @PathVariable (value = "postId") Long postId,
+                                 @Valid @RequestBody comment comment) throws UserNotFoundException {
+        if(!userRepo.existsById(userId)){
+            throw new UserNotFoundException(userId);
+        }
+        else{
+            return postRepo.findById(postId).map(post -> {
+                comment.setUser(userRepo.findByID(userId));
+                comment.setPost(post);
+                return commentRepo.save(comment);
+            }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
+        }
+    }
+
+    /**********************/
+    /** END POST Comment **/
+    /**********************/
+
+    //*****************//
+
+    /*********************/
+    /**** PUT Comments ***/
+    /*********************/
+
+    //Update comment with userId and postId
+    @PutMapping("{userId}/{postId}/updateComment/{commentId}")
+    public comment updateComment(@PathVariable (value = "userId") Long userId,
+                                 @PathVariable (value = "postId") Long postId,
+                                 @PathVariable (value = "commentId") Long commentId,
+                                 @Valid @RequestBody comment commentRequest) throws UserNotFoundException {
+        System.out.println(userRepo.findByUserName("hr24612"));
+        if(!userRepo.existsById(userId)) {
+            throw new UserNotFoundException(userId);
+        }
+        else if(!postRepo.existsById(postId)) {
+            throw new ResourceNotFoundException("PostId " + postId + " not found");
+        }
+        else{
+            return commentRepo.findById(commentId).map(comment -> {
+                comment.setText(commentRequest.getText());
+                return commentRepo.save(comment);
+            }).orElseThrow(() -> new ResourceNotFoundException("CommentId " + commentId + "not found"));
+        }
+    }
+
+    /*********************/
+    /** END PUT Comment **/
+    /*********************/
+
+    //*****************//
+
+    /*********************/
+    /*** DELETE Comment **/
+    /*********************/
+
+    //Delete a comment with userId and postId
+    @DeleteMapping("{userId}/{postId}/comment/{commentId}")
+    public ResponseEntity<?> deleteComment(@PathVariable (value = "userId") Long userId,
+                                           @PathVariable (value = "postId") Long postId,
+                                           @PathVariable (value = "commentId") Long commentId) throws UserNotFoundException {
+
+            return commentRepo.findById(commentId).map(comment -> {
+                if(comment.getUser().getId().equals(userId) && comment.getPost().getId().equals(postId)){
+                    commentRepo.delete(comment);
+                   return new ResponseEntity<>("Deleted!", HttpStatus.OK);
+                }
+                else {
+                    throw new ResourceNotFoundException("userId: " + userId + " or " + " postId: " + postId + " were incorrect");
+                }
+            }).orElseThrow(() -> new ResourceNotFoundException("Comment not found with id " + commentId));
+    }
+
+    /************************/
+    /** END DELETE Comment **/
+    /************************/
+
+    //*****************//
+
+}
--- Backend/app/src/main/java/com/example/app/model/user.java
+++ Backend/app/src/main/java/com/example/app/model/user.java
@@ -1,65 +1,65 @@
-//package com.example.app.model;
-//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//import javax.persistence.*;
-//import javax.validation.constraints.NotBlank;
-//import java.io.Serializable;
-//
-///**
-// * Class that represents a user
-// */
-//
-//@Entity
-//@Table(name = "user")
-//@EntityListeners(AuditingEntityListener.class)
-//public class user implements Serializable {
-//
-//    //DB column to save user id
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    //DB column to save user's firstName
-//    @NotBlank
-//    private String firstName;
-//
-//    //DB column to save user's lastName
-//    @NotBlank
-//    private String lastName;
-//
-//    //DB column to save user's userName
-//    @NotBlank
-//    private String userName;
-//
-//    //Constructor to initialize DB columns
-//    public user(Long id, String firstName, String lastName, String userName) {
-//        this.id = id;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.userName = userName;
-//    }
-//
-//    //Empty constructor to help with GET requests
-//    public user(){ }
-//
-//    //*****************//
-//
-//    /*************************/
-//    /** Getters and Setters **/
-//    /*************************/
-//
-//    public Long getId() { return id; }
-//    public void setId(Long id) { this.id = id; }
-//    public String getFirstName() { return firstName; }
-//    public void setFirstName(String firstName) { this.firstName = firstName; }
-//    public String getLastName() { return lastName; }
-//    public void setLastName(String lastName) { this.lastName = lastName; }
-//    public String getUserName() { return userName; }
-//    public void setUserName(String userName) { this.userName = userName; }
-//
-//    /*****************************/
-//    /** END Getters and Setters **/
-//    /*****************************/
-//
-//    //*****************//
-//
-//}
+package com.example.app.model;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+/**
+ * Class that represents a user
+ */
+
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
+public class user implements Serializable {
+
+    //DB column to save user id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //DB column to save user's firstName
+    @NotBlank
+    private String firstName;
+
+    //DB column to save user's lastName
+    @NotBlank
+    private String lastName;
+
+    //DB column to save user's userName
+    @NotBlank
+    private String userName;
+
+    //Constructor to initialize DB columns
+    public user(Long id, String firstName, String lastName, String userName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+    }
+
+    //Empty constructor to help with GET requests
+    public user(){ }
+
+    //*****************//
+
+    /*************************/
+    /** Getters and Setters **/
+    /*************************/
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
+
+    /*****************************/
+    /** END Getters and Setters **/
+    /*****************************/
+
+    //*****************//
+
+}
