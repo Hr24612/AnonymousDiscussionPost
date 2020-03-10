2bcfaae5c7c979924671f7335cc6676f9458e495
Matthew <mphipps@iastate.edu>
2020-03-09 21:45:14 -0500

    Fixed create user

--- Frontend/app/src/main/java/com/example/anti_social/net_utils/Const.java
+++ Frontend/app/src/main/java/com/example/anti_social/net_utils/Const.java
@@ -6,8 +6,8 @@ package com.example.anti_social.net_utils;
 public class Const {
     public static final String POSTMAN_URL = "https://postman-echo.com/get?foo1=bar1&foo2=bar2";
     public static final String LOCAL_HOST = "http://10.0.2.2";
-    public static final String CREATEPOST = "http://coms-309-sk-4.cs.iastate.edu:8080/postApi/createPost";
     public static final String GETALLPOSTS = "http://coms-309-sk-4.cs.iastate.edu:8080/api/post/getAllPosts";
+    public static final String CREATEUSER = "http://coms-309-sk-4.cs.iastate.edu:8080/api/user/createUser";
 
     public static String getUserByName(String name){
         return "http://coms-309-sk-4.cs.iastate.edu:8080/api/user/getUserByUserName/"+name;
